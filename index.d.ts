78ff0f87e6e53b962abda2b8d43dff135e7c399b
Simon <carbontwelve@users.noreply.github.com>
2023-03-03 17:08:13 +0000

    feat: initial commit

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+type EleventyPluginInterlinkOptions = {
+  // defaultLayout is the optional default layout you would like to use for wrapping your embeds.
+  defaultLayout?: string,
+
+  // layoutKey is the front-matter value used for a per embed template, if found it will replace defaultLayout for
+  // that embed. This will always default to `embedLayout`.
+  layoutKey?: string,
+
+  // unableToLocateEmbedFn is invoked when an embed is unable to be found, this is normally due to a typo in the
+  // slug that you are using. This defaults to a function that returns [UNABLE TO LOCATE EMBED].
+  unableToLocateEmbedFn?: ErrorRenderFn,
+
+  // slugifyFn is used to slugify strings. If a function isn't set then the default 11ty slugify filter is used.
+  slugifyFn?: SlugifyFn
+}
+
+interface ErrorRenderFn {
+  (slug: string): string;
+}
+
+interface SlugifyFn {
+  (input: string): string;
+}
+
+export {EleventyPluginInterlinkOptions};
