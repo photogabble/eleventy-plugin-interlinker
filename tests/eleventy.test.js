4a987cd826bb8390200fd6f6a91aae89ea8c0db1
Simon <carbontwelve@users.noreply.github.com>
2024-04-30 18:40:33 +0100

    chore: update test to be aware of new fixture structure...
    ... this tests both the wikilink parsing and the html link parsing.

--- tests/eleventy.test.js
+++ tests/eleventy.test.js
@@ -1,22 +1,28 @@
 const test = require("ava");
 const Eleventy = require("@11ty/eleventy");
-const{normalize} = require('./helpers');
+const {normalize} = require('./helpers');
 
-test("Sample page (wikilink from markdown to liquid)", async t => {
+function findResultByUrl(results, url) {
+  const [result] = results.filter(result => result.url === url);
+  return result;
+}
+
+test("Sample page (wikilinks and regular links)", async t => {
   let elev = new Eleventy(`${__dirname}/fixtures/sample-small-website/`, `${__dirname}/fixtures/sample-small-website/_site`, {
-    configPath: `${__dirname}/fixtures/sample-small-website/eleventy.config.js`
+    configPath: `${__dirname}/fixtures/sample-small-website/eleventy.config.js`,
   });
 
   let results = await elev.toJSON();
 
   t.is(2, results.length);
 
-  const [result] = results.filter(result => result.url === '/about/');
+  t.is(
+    `<div><p>This is to show that we can link between Markdown files and <a href="/hello/">liquid files</a>.</p></div><div><a href="/hello/">Hello</a></div>`,
+    normalize(findResultByUrl(results, '/about/').content)
+  );
 
   t.is(
-    '<p>This is to show that we can link between Markdown files and <a href="/hello/">liquid files</a>.</p>',
-    normalize(result.content)
+    '<div>This is to show that we can link back via <a href="/about">regular <em>internal</em> links</a>.</div><div><a href="/about/">About</a></div>',
+    normalize(findResultByUrl(results, '/hello/').content)
   );
 });
-
-
